11d847a6eef747432a09650cf961347978cce4dc
Miarantsoa Ainaharison <miarantsoaainaharison@gmail.com>
2024-07-16 08:42:31 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/my-app/S4/Web/Projets/Garage';
+
+$config['base_url'] = '/projets/CodeIgniter/Garage';
 
 /*
 |--------------------------------------------------------------------------
